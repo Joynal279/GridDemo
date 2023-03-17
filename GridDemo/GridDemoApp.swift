d46e16e030ad0da85d03584c27897939ef164d65
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-03-17 08:43:42 +0600

    Initial Commit

---
+++ GridDemo/GridDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridDemoApp.swift
+//  GridDemo
+//
+//  Created by Joynal Abedin on 17/3/23.
+//
+
+import SwiftUI
+
+@main
+struct GridDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
